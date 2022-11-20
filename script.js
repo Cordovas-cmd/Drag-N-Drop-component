b578c077f627167134b8f92d38401b70b88f050d
Samuel Cordova <cordovas1337@gmail.com>
2022-11-19 23:33:33 -0600

    added the functions I'd like to associate with the events that fire off with just console logs for the time being for testing

---
+++ script.js
@@ -0,0 +1,22 @@
+const fill = document.querySelector('.fill')
+const empty = document.querySelectorAll('.empty')
+
+
+function dragStart() {
+    console.log('Drag Start')
+}
+function dragEnd() {
+    console.log('Drag End')
+}
+function dragOver() {
+    console.log('Drag Over')
+}
+function dragEnter() {
+    console.log('Drag Enter')
+}
+function dragLeave() {
+    console.log('Drag Leave')
+}
+function dragDrop() {
+    console.log('Drag Drop')
+}
