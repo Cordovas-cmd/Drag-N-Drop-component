075a5f6a2e5745581bc20402d99deca7c8490195
Samuel Cordova <cordovas1337@gmail.com>
2022-11-19 23:51:25 -0600

    added most of the functionality for dragging and dropping one background image from one box to the next

--- script.js
+++ script.js
@@ -13,25 +13,36 @@ for(const empty of empties) {
 }
 
 function dragStart() {
-    console.log('Drag Start')
+    this.className += ' hold'
+    setTimeout(() => this.className = 'invisible', 0)
 }
 
 function dragEnd() {
-    console.log('Drag End')
+    this.className = 'fill'
 }
 
-function dragOver() {
+// add prevent default as per mdn guidelines ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dragover_event
+
+function dragOver(e) {
+    e.preventDefault()
     console.log('Drag Over')
 }
 
-function dragEnter() {
+
+function dragEnter(e) {
+    e.preventDefault()
+    this.className+= ' hovered'
     console.log('Drag Enter')
 }
 
 function dragLeave() {
+    this.className = 'empty'
     console.log('Drag Leave')
 }
 
 function dragDrop() {
-    console.log('Drag Drop')
+//    make sure it's empty before we drop.
+    this.className= 'empty'
+    this.append(fill)
+   
 }
