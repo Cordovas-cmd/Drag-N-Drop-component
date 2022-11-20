23032be1537b2dc6322e2548a50424ee7f4bd3cb
Samuel Cordova <cordovas1337@gmail.com>
2022-11-19 23:40:39 -0600

    added the event listeners and a for of loop for the drag events and functions on empty boxes

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 const fill = document.querySelector('.fill')
-const empty = document.querySelectorAll('.empty')
+const empties = document.querySelectorAll('.empty')
 
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+
+// for of loop (node list)
+for(const empty of empties) {
+    empty.addEventListener('dragover', dragOver)
+    empty.addEventListener('dragenter', dragEnter)
+    empty.addEventListener('dragleave', dragLeave)
+    empty.addEventListener('drop', dragDrop)
+}
 
 function dragStart() {
     console.log('Drag Start')
 }
+
 function dragEnd() {
     console.log('Drag End')
 }
+
 function dragOver() {
     console.log('Drag Over')
 }
+
 function dragEnter() {
     console.log('Drag Enter')
 }
+
 function dragLeave() {
     console.log('Drag Leave')
 }
+
 function dragDrop() {
     console.log('Drag Drop')
 }
